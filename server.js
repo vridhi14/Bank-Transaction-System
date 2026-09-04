9886613339180eacd6123206c7ad912c1c2839b3
vridhi <rajeevvridhi@gmail.com>
2026-09-04 11:57:16 +0530

    feat: add dotenv support and mongoose connection for database integration

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+require("dotenv").config(); 
 const app = require("./src/app"); 
+const connectToDb = require("./src/config/db");
 
 app.listen(3000 , ()=>{
     console.log("server is running on port 3000"); 
---
+++ src/config/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose"); 
+
+function connectToDb(){
+    mongoose.connect(process.env.MONGODB_URI)
+    .then(()=>console.log("server is connected to DB"))
+    .catch((err) =>{
+        console.log("error connecting to db")
+        process.exit(1); 
+    }); 
+}
+module.exports = connectToDb
