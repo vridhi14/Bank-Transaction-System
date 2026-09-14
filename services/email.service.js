0ad0a7d1a170dfb39b4156211ab5dd1e46060669
vridhi <rajeevvridhi@gmail.com>
2026-09-14 17:54:46 +0530

    feat: implement sendEmail function for email sending functionality

--- services/email.service.js
+++ services/email.service.js
@@ -21,4 +21,24 @@ transporter.verify((error, success) => {
   }
 });
 
+// Function to send email
+const sendEmail = async (to, subject, text, html) => {
+  try {
+    const info = await transporter.sendMail({
+      from: `"Bank Transaction System" <${process.env.EMAIL_USER}>`, // sender address
+      to, // list of receivers
+      subject, // Subject line
+      text, // plain text body
+      html, // html body
+    });
+
+    console.log('Message sent: %s', info.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  } catch (error) {
+    console.error('Error sending email:', error);
+  }
+};
+
+module.exports = sendEmail;
+
 module.exports = transporter;
