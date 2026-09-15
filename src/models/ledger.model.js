c12dbd61543ffac8c9b85175ebcef88730f5fda2
vridhi <rajeevvridhi@gmail.com>
2026-09-15 18:43:16 +0530

    feat: add ledger model with schema definition and immutability enforcement

---
+++ src/models/ledger.model.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose"); 
+const ledgerSchema = new mongoose.Schema({
+    account : {
+        type : mongoose.Schema.Types.objectId , 
+        ref : "account", 
+        required : [true , "ledger must be associated with an account"],
+        index : true , 
+        immutable : true  
+    }, 
+    amount : {
+        type : "Number", 
+        required : [true , "Amount is required for creating a ledger entry "], 
+        immutable : true , 
+    }, 
+    transaction : {
+        type : mongoose.Schema.Types.objectId , 
+        ref : "transaction", 
+        required : [true , "ledger must be associated with a transaction"],
+        index : true , 
+        immutable : true 
+    }, 
+    type : {
+        type : String , 
+        enum : {
+            values : ["CREDI" , "DEBIT"], 
+            message : "Type can be either CREDIT or DEBIT", 
+        }, 
+        required : [true , "Ledger type is required"], 
+        immutable:true 
+    }
+})
+
+function preventLedgerModification(){
+    throw new Error("Ledger entries are immutable and cannot be modified or deleted"); 
+}
+
