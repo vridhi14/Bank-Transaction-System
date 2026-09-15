ae9348de5b4814c7aa04dd13412dc833018bfa03
vridhi <rajeevvridhi@gmail.com>
2026-09-15 17:36:56 +0530

    refactor: reorganize route imports in app.js and add account controller

--- src/app.js
+++ src/app.js
@@ -1,17 +1,18 @@
 const express = require('express'); 
 const cookieParser = require("cookie-parser");
 
-const authRouter = require('./routes/auth.routes')
-const accountRouter = require('./routes/accounts.routes')
-
-
 const app = express(); 
-
 
 //Middlewares: 
 app.use(express.json());
 app.use(cookieParser());
 
+// Routes Required
+const authRouter = require('./routes/auth.routes')
+const accountRouter = require('./routes/accounts.routes')
+
+//use Routes
 app.use("/api/auth" , authRouter); 
+app.use("/api/acounts" , accountRouter); 
 
 module.exports = app ; 
---
+++ src/controllers/account.controller.js
@@ -0,0 +1,1 @@
+const accountModel = require("../models/account.model");
