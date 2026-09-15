5ee88e8913a8233f8c11454485d9191a020a346e
vridhi <rajeevvridhi@gmail.com>
2026-09-15 18:49:22 +0530

    fix: enhance immutability enforcement on ledger entries by preventing additional modification operations

--- src/models/ledger.model.js
+++ src/models/ledger.model.js
@@ -39,6 +39,9 @@ ledgerSchema.pre('updateOne' ,preventLedgerModification );
 ledgerSchema.pre('deleteOne' ,preventLedgerModification );
 ledgerSchema.pre('remove' ,preventLedgerModification );
 ledgerSchema.pre('deleteMany' , preventLedgerModification);
+ledgerSchema.pre('updateMany' , preventLedgerModification);
+ledgerSchema.pre('findOneAndDelete' , preventLedgerModification);
+ledgerSchema.pre('findOneAndReplace' , preventLedgerModification);
 
 const ledgerModel = mongoose.model('ledger' , ledgerSchema); 
 module.exports = ledgerModel ; 
