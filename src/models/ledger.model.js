170dd7cb807d5fa8e84a9b1f4f4ebd18d28580f7
vridhi <rajeevvridhi@gmail.com>
2026-09-15 18:45:49 +0530

    fix: enforce immutability on ledger entries by preventing modification and deletion

--- src/models/ledger.model.js
+++ src/models/ledger.model.js
@@ -34,3 +34,8 @@ function preventLedgerModification(){
     throw new Error("Ledger entries are immutable and cannot be modified or deleted"); 
 }
 
+ledgerSchema.pre('findOneAndUpdate' ,preventLedgerModification );
+ledgerSchema.pre('updateOne' ,preventLedgerModification );
+ledgerSchema.pre('deleteOne' ,preventLedgerModification );
+ledgerSchema.pre('remove' ,preventLedgerModification );
+ledgerSchema.pre('deleteMany' , preventLedgerModification);
