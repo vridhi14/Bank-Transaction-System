bab872136a49405bbb1b70bf0e442c1ba4128000
vridhi <rajeevvridhi@gmail.com>
2026-09-17 14:00:38 +0530

    refactor: update server initialization to use environment variable for port

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-require("dotenv").config(); 
-const app = require("./src/app"); 
-const connectToDb = require("./src/config/db.js");
+require("dotenv").config()
 
-app.listen(3000 , ()=>{
-    console.log("server is running on port 3000"); 
+const app = require("./src/app")
+const connectToDB = require("./src/config/db")
+
+connectToDB()
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
 })
-connectToDb(); 
