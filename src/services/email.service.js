42bea0f55597b925f0dac2ef5b406fd88b506538
vridhi <rajeevvridhi@gmail.com>
2026-09-17 13:49:23 +0530

    refactor: simplify email transporter configuration by removing OAuth2 settings

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,14 +1,22 @@
 require('dotenv').config();
 const nodemailer = require('nodemailer');
 
+// const transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     type: 'OAuth2',
+//     user: process.env.EMAIL_USER,
+//     clientId: process.env.CLIENT_ID,
+//     clientSecret: process.env.CLIENT_SECRET,
+//     refreshToken: process.env.REFRESH_TOKEN,
+//   },
+// });
+
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    type: 'OAuth2',
     user: process.env.EMAIL_USER,
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    refreshToken: process.env.REFRESH_TOKEN,
+    pass: process.env.EMAIL_PASS,
   },
 });
 
