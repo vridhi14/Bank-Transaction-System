9c4a9a2be27686b832e37afb72f1936842350480
vridhi <rajeevvridhi@gmail.com>
2026-09-17 13:57:24 +0530

    feat: add transaction routes and update root endpoint response

--- src/app.js
+++ src/app.js
@@ -10,9 +10,13 @@ app.use(cookieParser());
 // Routes Required
 const authRouter = require('./routes/auth.routes')
 const accountRouter = require('./routes/accounts.routes')
+const transactionRoutes = require('./routes/transaction.routes')
 
 //use Routes
+app.get("/",(req,res)=>{
+    res.send("Bank Transaction Sytem is running")
+})
 app.use("/api/auth" , authRouter); 
 app.use("/api/acounts" , accountRouter); 
-
+app.use("/api/transactions" , transactionRoutes); 
 module.exports = app ; 
