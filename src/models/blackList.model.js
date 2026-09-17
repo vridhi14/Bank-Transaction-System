7f1a4cb5eb35925264ebe79aab650c8dde9e792e
vridhi <rajeevvridhi@gmail.com>
2026-09-17 12:40:08 +0530

    feat: add token blacklist model to manage user logout tokens

---
+++ src/models/blackList.model.js
@@ -0,0 +1,20 @@
+//user logout = blacklist token so that any other user will not be able to use this token 
+
+const mongoose = require("mongoose"); 
+const tokenBlacklistSchema = new mongoose.Schema({
+    token: {
+        type: String,
+        required: [ true, "Token is required to blacklist" ],
+        unique: [ true, "Token is already blacklisted" ]
+    }
+}, {
+    timestamps: true
+})
+
+tokenBlacklistSchema.index({ createdAt: 1 }, {
+    expireAfterSeconds: 60 * 60 * 24 * 3 //3 days 
+    })
+
+const tokenBlackListModel = mongoose.model("tokenBlackList", tokenBlacklistSchema);
+
+module.exports = tokenBlackListModel;
